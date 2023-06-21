a1d3ded578c0d08b56046357b9a7ea68600e3611
Lucas Job Viana <lucasjobvianna@gmail.com>
2023-06-20 21:37:53 -0300

    Adiconando rotas.

---
+++ src/AppRouter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
+import App from './page/App';
+
+
+
+function AppRouter() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={ App } />
+      </Switch>
+        
+    </div>
+  );
+}
+
+export default AppRouter; 
---
+++ src/page/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../component/Header';
+import Main from '../component/Main';
+import '../style/App.css';
+
+const App = () =>  {
+  return (
+    <>
+        <Header />
+        <Main />
+    </>
+  );
+}
+
+export default App;
