342d18cf336ba3bd67a6fa9841633042211ed388
Lucas Job Viana <lucasjobvianna@gmail.com>
2023-09-27 00:14:54 -0300

    :sparkles: feat: Alterado ProjectConteiner para usar componentes mui.

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import { Switch, Route, useHistory, useLocation } from 'react-router-dom/cjs/react-router-dom.min';
-import Header from './component/Header';
+// import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { Switch, Route, useLocation } from 'react-router-dom/cjs/react-router-dom.min';
+// import Header from './component/Header';
 import App from './page/App';
 import Projects from './page/Projects';
 // import Tools from './page/Tools';
@@ -12,7 +12,7 @@ import './style/App.css';
 function AppRouter() {
 	const { user: { buttonsNav } } = useSelector((store) => store);
 	const pathofThisPage = useLocation().pathname;
-	const history = useHistory();
+	// const history = useHistory();
 	let index = '66';
 
 	for (const [key, value] of Object.entries(buttonsNav)) {
@@ -23,42 +23,42 @@ function AppRouter() {
 	}
 	console.log('ja achei meu index = ', index)
 
-	const handleClick = (page) => {
-		console.log('meu index', index)
-		let numberIndex = Number(index);
-		if (page === 'prev') {
-			numberIndex = numberIndex === 0 ? (buttonsNav.length) : numberIndex;
-			history.push(buttonsNav[--numberIndex].to);
-			return;
-		}
-		numberIndex = numberIndex === (buttonsNav.length) - 1 ? -1 : numberIndex;
-		history.push(buttonsNav[++numberIndex].to)
-	}
+	// const handleClick = (page) => {
+	// 	console.log('meu index', index)
+	// 	let numberIndex = Number(index);
+	// 	if (page === 'prev') {
+	// 		numberIndex = numberIndex === 0 ? (buttonsNav.length) : numberIndex;
+	// 		history.push(buttonsNav[--numberIndex].to);
+	// 		return;
+	// 	}
+	// 	numberIndex = numberIndex === (buttonsNav.length) - 1 ? -1 : numberIndex;
+	// 	history.push(buttonsNav[++numberIndex].to)
+	// }
 
 
 	return (
 		<div className="App">
-			<Header />
+			{/* <Header /> */}
 			<div className='app'>
-				<button onClick={() => { handleClick('prev') }} className='btn-direction prev-page' />
-				<button onClick={() => { handleClick('next') }} className='btn-direction nav next-page' />
+				{/* <button onClick={() => { handleClick('prev') }} className='btn-direction prev-page' />
+				<button onClick={() => { handleClick('next') }} className='btn-direction nav next-page' /> */}
 
-				<Route render={({ location }) => (
+				<Route render={() => (
 
-					<TransitionGroup>
-						<CSSTransition
-							key={location.key}
-							timeout={400}
-							classNames="fade"
-						>
+					// <TransitionGroup>
+					// 	<CSSTransition
+					// 		key={location.key}
+					// 		timeout={400}
+					// 		classNames="fade"
+					// 	>
 							<Switch>
 								<Route exact path="/" component={App} />
 								<Route exact path="/projects" component={Projects} />
 								{/* <Route exact path="/tools" component={Tools} />
 								<Route exact path="/about" component={About} /> */}
 							</Switch>
-						</CSSTransition>
-					</TransitionGroup>
+					// 	</CSSTransition>
+					// </TransitionGroup>
 				)}
 				/>
 			</div>
--- src/component/ProjectConteiner.jsx
+++ src/component/ProjectConteiner.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import ProjectCard from './ProjectCard';
+// import ProjectCard from './ProjectCard';
 import { useSelector } from 'react-redux'; 
- 
+import { Button, Card, CardActions, CardContent, CardMedia, Container, Grid, Typography } from '@mui/material';
+import { GitHub, OpenInNew } from '@mui/icons-material'
+
 const ProjectConteiner = () => {
   const { user: { iconLinks } } = useSelector((store) => store);
   const { project: {projects} } = useSelector((store) => store);
@@ -9,13 +11,46 @@ const ProjectConteiner = () => {
   console.log(iconLinks);
  
   return (
-    <div className='projects-container'>{
-      projects.map((p,index)=>(
-        <ProjectCard key={`project${index}`} project={p}  />
-      ))
-    }
-      
-    </div>
+    <Container sx={{ py: 8 }} maxWidth="md">
+
+  <Grid container spacing={4}>
+    {projects.map((card) => (
+      <Grid item key={card} xs={12} sm={6} md={4}>
+        <Card
+          sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+        >
+          <CardMedia
+            component="div"
+            sx={{
+              // 16:9
+              pt: '56.25%',
+            }}
+            image="https://source.unsplash.com/random?wallpapers"
+          />
+          <CardContent sx={{ flexGrow: 1 }}>
+            <Typography gutterBottom variant="h5" component="h2">
+              { card.title}
+            </Typography>
+            <Typography>
+              { card.description }
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small" startIcon={<GitHub />} >Code</Button>
+            <Button size="small" startIcon={<OpenInNew />}>Deploy</Button>
+          </CardActions>
+        </Card>
+      </Grid>
+    ))}
+  </Grid>
+</Container> 
+      // <div className='projects-container'>{
+      //   projects.map((p,index)=>(
+      //     <ProjectCard key={`project${index}`} project={p}  />
+      //   ))
+      // }
+        
+      // </div>
   )
 }
 
